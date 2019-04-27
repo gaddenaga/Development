a93a077e9d783071c5e60212e9e3783c353afe38
Nagaraj <Nagaraj@DESKTOP-HI6IBQD>
2019-04-27 13:43:24 +0530

    testingfile

---
+++ WebApplication/src/bean/User.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package bean;
+
+/**
+ * @author Nagaraj test
+ *
+ */
+public class User {
+
+	private String uname;
+	private String upass;
+	private String uemail; 
+	private int id;
+	/**
+	 * @return the uname
+	 */
+	public String getUname() {
+		return uname;
+	}
+	/**
+	 * @param uname the uname to set
+	 */
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	/**
+	 * @return the upass
+	 */
+	public String getUpass() {
+		return upass;
+	}
+	/**
+	 * @param upass the upass to set
+	 */
+	public void setUpass(String upass) {
+		this.upass = upass;
+	}
+	/**
+	 * @return the uemail
+	 */
+	public String getUemail() {
+		return uemail;
+	}
+	/**
+	 * @param uemail the uemail to set
+	 */
+	public void setUemail(String uemail) {
+		this.uemail = uemail;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+
+}
